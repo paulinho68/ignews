0641a3b6af58e3c852054b505144ce542ae0c179
Paulo Vitor <paulovitor2123@gmail.com>
2021-04-08 01:46:14 -0300

    First layout - home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Teste
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>For $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Firl coding" />
+      </main>
     </>
   )
 }
